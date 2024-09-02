3cb18b89f1aa5bb0b98c1e57832b66d4bc298840
Harry Gray <harry.gray@environment-agency.gov.uk>
2024-09-02 13:34:44 +0000

    Changed leaflet maps so forced to toggle off polygons when slecting new layer. Plust added source( ) to top of each .qmd instead of copy paste CAT codes

---
+++ Catch_Set_Up.R
@@ -0,0 +1,15 @@
+catch <- read_sf("/dbfs/mnt/lab/unrestricted/harry.gray@environment-agency.gov.uk/Interim_WFD_2022.shp")# Catchment shapefiles
+CAT <- catch[catch$OPCAT_NAME == "Parrett",]
+
+
+CAT_Union <- st_union(CAT) %>% 
+  st_transform(4326)
+
+CAT %<>% st_transform(4326)
+
+
+CPS <- read.csv("/dbfs/mnt/lab/unrestricted/harry.gray@environment-agency.gov.uk/CEP/CPS_Exports/WSX_rnags_01082024.csv")
+
+CAT_geo <- subset(CAT, select = c(WB_ID, geometry))
+
+CPS_sf <- inner_join(CAT_geo, CPS, by = "WB_ID")
