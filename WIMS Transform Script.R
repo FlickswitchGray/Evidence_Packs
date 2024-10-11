ef653ec0cafad74a216c60196dee43a062c104c1
harry gray <harry.gray@environment-agency.gov.uk>
2024-10-11 11:39:05 +0000

    Finalised

--- WIMS Transform Script.R
+++ WIMS Transform Script.R
@@ -1,9 +1,23 @@
 # WIMS Transform Script
 
     WIMS <-  read.csv("/dbfs/FileStore/WSX_HGray/ETL_Exports/Wessex_WIMS_AllDeters_091024.csv")
+
+    WIMS %<>% st_as_sf(coords= c("sample.samplingPoint.easting", "sample.samplingPoint.northing"), crs=27700) %>% 
+      filter(!is.na("sample.samplingPoint.easting") &!is.na("sample.samplingPoint.northing")) 
     
-    WIMS %<>% st_as_sf(coords, c("sample.samplingPoint.easting", "sample.samplingPoint.northing"), crs=27700) %>% 
-      st_transform(4326)
+    # Transform CAT so can join in planar geoms.  
+    CAT_W <-st_transform(CAT, st_crs(27700))
     
+    # Spatial join
+    joined <- st_join(WIMS, CAT_W)
+ 
+    joined_w <- st_transform(joined, st_crs(4326))
     
+    # Crop Wessex wide to be in catchment.
+    WIMS_CAT <- joined[CAT_W,]
     
+    # 
+    WIMS_CAT %<>% st_transform(st_crs(4326))
+  
+
+      
